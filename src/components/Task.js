c1cb163bac0ac79c3f40e8e8abaa190551cf0656
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-03-14 21:51:02 +0100

    Change display data in Task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,31 @@
 import React from "react";
 
 const Task = props => {
-  const { id, text, date } = props.task;
-  return (
-    <div>
-      <div>{text}</div>
-      <div>{date}</div>
-      <button onClick={() => props.status(id)}>DONE</button>
-      <button onClick={() => props.delete(id)}>X</button>
-    </div>
-  );
-};
+  const style = {
+    color: "red"
+  };
+  const { id, text, date, status, important, finishDate } = props.task;
 
+  if (status) {
+    return (
+      <div>
+        <div style={important ? style : null}>{text}</div>
+        <div>zrobić do: {date}</div>
+        <button onClick={() => props.status(id)}>DONE</button>
+        <button onClick={() => props.delete(id)}>X</button>
+      </div>
+    );
+  } else {
+    const finishTime = new Date(finishDate).toLocaleString();
+
+    return (
+      <div>
+        <div>{text}</div>
+        <div>zrobić do: {date}</div>
+        <div>wykonano: {finishTime}</div>
+        <button onClick={() => props.delete(id)}>X</button>
+      </div>
+    );
+  }
+};
 export default Task;
